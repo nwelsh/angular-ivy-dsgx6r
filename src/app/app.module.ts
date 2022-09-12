05eefbcba5ca6b139df773fcedf1d183af1495aa
nwelsh <newelsh@wisc.edu>
2022-09-12 13:27:10 -0500

    action component working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
+import { ActionComponent } from './action/action.component';
+import { NetworkComponent } from './network/network.component';
+import { NotificationComponent } from './notification/notification.component';
+import { MeComponent } from './me/me.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, ActionComponent, NetworkComponent, NotificationComponent, MeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
